5f49401e345b26ba47334e12b7ec9c04a2f82bd9
Guslington <guslington@gmail.com>
2022-04-21 12:03:08 +1000

    bump r53 component version due to python lambda runtime 3.6 end of life

--- vpc-v2.cfhighlander.rb
+++ vpc-v2.cfhighlander.rb
@@ -64,7 +64,7 @@ CfhighlanderTemplate do
     
   end
   
-  Component template: 'route53-zone@1.0.2', name: 'dnszone', render: Inline do
+  Component template: 'route53-zone@1.0.3', name: 'dnszone', render: Inline do
     parameter name: 'CreateZone', value: 'true'
     parameter name: 'RootDomainName', value: FnSub('${DnsDomain}.')
   end if manage_ns_records && create_hosted_zone
