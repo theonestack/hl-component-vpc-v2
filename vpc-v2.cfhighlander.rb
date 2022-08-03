3f6121032aded5d1808b5675ce3e8ebbc1163f91
Aaron Walker <a.walker@base2services.com>
2022-08-03 15:15:08 +0000

    allows overriding the dns hosted zone naming

--- vpc-v2.cfhighlander.rb
+++ vpc-v2.cfhighlander.rb
@@ -64,9 +64,10 @@ CfhighlanderTemplate do
     
   end
   
-  Component template: 'route53-zone@1.0.3', name: 'dnszone', render: Inline do
+  Component template: 'route53-zone@1.4.0', name: 'dnszone', render: Inline do
     parameter name: 'CreateZone', value: 'true'
     parameter name: 'RootDomainName', value: FnSub('${DnsDomain}.')
+    parameter name: 'DnsDomain', value: Ref(:DnsDomain)
   end if manage_ns_records && create_hosted_zone
 
 end
